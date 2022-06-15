c3c9ed2a2a64ef1068bb99060d854d645442a4bb
Natan Brito <natanismaelbrito@gmail.com>
2022-06-15 16:55:23 -0300

    CHORE designing the feature for the client to publish something, almost done design and missing js to implement

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
 import Template from "../pages/Template";
-import UserPublish from "../components/UserPublish";
-
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Template />} />
-                <Route path="/buildpost" element={<UserPublish />} />
+                <Route path="/timeline" element={<Template />} />
             </Routes>
         </BrowserRouter>
     );
