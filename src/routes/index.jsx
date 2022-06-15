3df94ec43b0c8476dbe7a125b6462fb146ffcb2f
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-06-15 03:43:16 -0300

    chore: deleting test files

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,15 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
 import Template from "../pages/Template";
-import Trending from "../components/Trending";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Template />} />
-                <Route path="/trending" element={<Trending />} />
-                <Route path="/hashtag/:hashtag" element={<Template />} />
             </Routes>
         </BrowserRouter>
     );
