b6ab5d44ccc6acb65d2ce2df5464ff539a1e602a
Natan Brito <natanismaelbrito@gmail.com>
2022-06-16 13:43:48 -0300

    refactor: adjustment publish make by user

--- src/components/UserPublish/styles.js
+++ src/components/UserPublish/styles.js
@@ -1,67 +1,79 @@
 import styled from "styled-components";
 
+export const Form = styled.form`
+    width: 100%;
+    height: 80%;
+`;
+export const Inputs = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
 export const BoxPublish = styled.div`
     display: flex;
+    justify-content: center;
 `;
 
 export const Container = styled.div`
     position: relative;
     margin-top: 5px;
     display: flex;
-    background-color: #ffffff;
-    max-width: 620px;
-    min-width: 375px;
-    max-height: 200px;
-    min-height: 160px;
+    background-color: var(--secondary-color);
+    width: 100%;
+    height: 160px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 16px;
     padding: 2px;
 `;
-export const teste = styled.div`
+export const Button = styled.div`
     display: flex;
     justify-content: flex-end;
     align-items: center;
     width: 100%;
     height: 20%;
-    background-color: black;
+    cursor: pointer;
+    // background-color: black;
     .PublishButton {
         width: 112px;
-        height: 31px;
+        height: 22px;
         border-radius: 5px;
         border: none;
-        background-color: var(--color-buttons);
+        background-color: var(--color-button);
         color: white;
         font-weight: bold;
     }
 `;
-export const Forms = styled.div`
-    width: 100%;
-    height: 80%;
-    background-color: blue;
-`;
-export const Right = styled.div`
-    width: 80%;
-    height: 100%;
-    margin-left: 10px;
-`;
-export const Left = styled.div`
-    width: 50px;
-    height: 100%;
-    border-radius: 8px;
-    background-color: red;
-`;
 
-export const ImageUser = styled.div`
-    position: absolute;
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    margin-left: 1%;
-    margin-top: 1%;
-    img {
+export const Data = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 95%;
+    height: 100%;
+    margin: auto;
+    p {
+        text-align: center;
+        color: #707070;
+        font-family: "Lato";
+        font-style: normal;
+        font-weight: 300;
+        font-size: 17px;
+        margin-top: 1%;
+        margin-bottom: 15px;
+    }
+    input {
+        background: #efefef;
+        border-radius: 5px;
+        border: none;
         width: 100%;
-        height: 100%;
-        border-radius: 50%;
-        object-fit: cover;
+        ::placeholder {
+            color: #949494;
+        }
+    }
+    .inputUrl {
+        height: 30px;
+    }
+    .inputArticle {
+        height: 47px;
+        margin-top: 5px;
+        margin-bottom: 5px;
     }
 `;
