ce8a10187cb8c56e8503729111cadbc58645d7a6
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-06-16 01:32:01 -0300

    fix: trending box components

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,16 @@ import React from "react";
 
 import Template from "../pages/Template";
 import UserPublish from "../components/UserPublish";
+
+import Trending from "../components/Trending";
+
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Template />} />
                 <Route path="/timeline" element={<UserPublish />} />
+                <Route path="/testes" element={<Trending />} />
             </Routes>
         </BrowserRouter>
     );
