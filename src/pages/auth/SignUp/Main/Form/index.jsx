82e9df029eaa9080a09ced100a0f777825812fc9
joaohdallago <joaohdallago@gmail.com>
2022-06-19 14:41:26 -0300

    feat: add error handler

--- src/pages/auth/SignUp/Main/Form/index.jsx
+++ src/pages/auth/SignUp/Main/Form/index.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+import handleError from "../../../../../utils/handleError";
 import * as S from "./../../../styles";
 import Inputs from "./Inputs";
 import SubmitButton from "./SubmitButton";
 
 const Form = () => {
+    const navigate = useNavigate();
+
     const [ signUpData, setSignUpData ] = useState({
         email: "",
         password: "",
@@ -14,8 +21,12 @@ const Form = () => {
 
     const signUp = (e) => {
         e.preventDefault();
-
-        alert();
+        const url = `${process.env.REACT_APP_URI}/sign-up`;
+        
+        axios
+            .post(url, signUpData)
+            .then(() => navigate("/sign-in"))
+            .catch(handleError);
     };
 
     return (
